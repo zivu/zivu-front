bb0b293e9d4615d0a6a0fdb9de2402672c63e10a
pavlo_zvarych <rj.pavlo.zvarych@gmail.com>
2022-09-10 15:59:28 +0200

    marks saved in map

--- src/app/questions-fetcher/questions-fetcher.component.ts
+++ src/app/questions-fetcher/questions-fetcher.component.ts
@@ -3,6 +3,7 @@ import { QuestionService } from '../question.service';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-questions-fetcher',
@@ -14,6 +15,8 @@ export class QuestionsFetcherComponent implements OnInit {
   questions!: Observable<{ question: string, answer: string, level: string, technology: string }[]>;
 
   requestUrl: string = 'http://localhost:8080/questions/selected';
+
+  private marks = new Map();
 
   constructor(private service: QuestionService, private httpClient: HttpClient) { 
     this.questions = this.requestQuestions();
@@ -33,6 +36,10 @@ export class QuestionsFetcherComponent implements OnInit {
       + '&has_spring=' + this.service.hasSpring()
       + '&has_js=' + this.service.hasJs()
       + '&has_sql=' + this.service.hasSql();
+  }
+
+  addMark(technology: String, event: Event, i: number): void {
+    this.marks.set(technology + '_' + i.toString(), (event.target as HTMLInputElement).value);
   }
 
 }
