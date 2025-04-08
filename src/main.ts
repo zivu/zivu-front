aa3274099985347e911f877eddb8b43bd135f6a4
rj.pavlo.zvarych <rj.pavlo.zvarych@gmail.com>
2025-04-08 09:30:37 +0000

    old angular fix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
+import { provideRoutes } from '@angular/router';
 import { appRoutes } from './app/app-routing.module';
 
 if (environment.production) {
@@ -10,6 +10,6 @@ if (environment.production) {
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes)],
+  providers: [provideRoutes(appRoutes)],
 })
   .catch((err) => console.error(err));
